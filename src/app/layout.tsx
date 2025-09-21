e0f636bd38f0916d66808561570c8b897aa92786
rbelleville2159 <remy.belleville@gmail.com>
2025-09-20 23:52:33 -0400

    Update layout.tsx
    fixed code for icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,11 @@ import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-      icons: {
-        icon: '/favicon.ico',
-      },
-    };
+export const metadata: Metadata = {
+  icons: {
+          icon: '/favicon.ico',
+         },
+}
 
 export const metadata: Metadata = {
   title: 'QuarterPong|Bounce.Compete.Eliminate.',
